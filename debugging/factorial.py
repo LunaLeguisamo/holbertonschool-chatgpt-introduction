f335af07c19f13f3e68d01c858cef2fa98642467
Luna Leguisamo <9356@holbertonstudents.com>
2024-09-02 14:07:11 -0300

    Factorial

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import sys
 
 def factorial(n):
@@ -7,13 +9,7 @@ def factorial(n):
         n -= 1  # Decrementamos n en cada iteración
     return result
 
-if len(sys.argv) > 1:  # Verificamos que se haya pasado un argumento
-    try:
         f = factorial(int(sys.argv[1]))
         print(f)
-    except ValueError:
-        print("Por favor, ingresa un número entero válido.")
-else:
-    print("Por favor, ingresa un número entero como argumento.")
 
 
